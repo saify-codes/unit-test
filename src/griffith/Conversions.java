82ce9a468659a4e010570e1258d6420429b1657a
Preshakpos7 <preshakpos8@gmail.com>
2024-03-16 01:58:34 +0500

    added method stubs

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -2,4 +2,34 @@ package griffith;
 
 public class Conversions {
 
+	// Method stubs
+	public double celsiusToFahrenheit(double celsius) {
+		// Implementation will be added later
+		return 0.0;
+	}
+
+	public double fahrenheitToCelsius(double fahrenheit) {
+		// Implementation will be added later
+		return 0.0;
+	}
+
+	public double milesToKilometer(double miles) {
+		// Implementation will be added later
+		return 0.0;
+	}
+
+	public double kilometerToMiles(double kilometers) {
+		// Implementation will be added later
+		return 0.0;
+	}
+
+	public String upperCaseToLowerCase(String input) {
+		// Implementation will be added later
+		return null;
+	}
+
+	public String lowerCaseToUpperCase(String input) {
+		// Implementation will be added later
+		return null;
+	}
 }
