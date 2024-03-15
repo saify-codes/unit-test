03cb1bcf8a9e22933c5db9680bbe530204006960
Preshakpos7 <preshakpos8@gmail.com>
2024-03-16 02:43:36 +0500

    add unit test for celsiusToFahrenheit method

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -1,6 +1,16 @@
 package griffith;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ConversionsTest {
 
-	// Test cases will be added later
+	@Test
+	public void testCelsiusToFahrenheit() {
+		Conversions converter = new Conversions();
+		assertEquals(32.0, converter.celsiusToFahrenheit(0), 0);
+		assertEquals(98.6, converter.celsiusToFahrenheit(37), 0);
+		assertEquals(-40.0, converter.celsiusToFahrenheit(-40), 0);
+	}
 }
